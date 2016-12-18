0dc9f7396014816e737e5599c769f97ab33a3e5b
matteo <delpiomat@hotmail.it>
2016-12-18 16:03:48 +0100

    -modifiy query for insert account

--- mail_to_select_sql.py
+++ mail_to_select_sql.py
@@ -1,8 +1,8 @@
-name_file = 'aziende_stageit.txt'
+name_file = 'mail_azz'
 
 #id auto increment ma a noi serve per la seconda insert
 id = 2
-password=''
+password='"pbkdf2_sha256$30000$ZxxNnT1er1h2$k7CsvSl2/AGyIibyQ9bgYqyuUAqPj7i/xwvV9XHwXS8="' #stageit
 is_superuser='0'
 username=''
 first_name=''
@@ -11,11 +11,15 @@ email=''
 is_staff='0'
 is_active='1'
 row=''
+date_joined='"2016-08-17 12:31:31.757389"'
 
 type = '1'
 azienda_id = 'null'
 survey_id = 'null'
 questionario = '0'
+activationCode = '"12345"'
+timeCode = '"2016-10-22 18:21:29.519471"'
+
 
 query=''
 
@@ -27,20 +31,20 @@ out_file = open("test.txt","w")
 with open(name_file) as fp:
     for line in fp:
         row = (line.strip()).lower()
-        email = str.split(row, ',')[1]
+        email = '"'+str.split(row, ',')[1]+'"'
         questionario = str.split(row, ',')[0]
         azienda_id = questionario
         username = email
-        first_name = email
-        last_name = email
+        first_name = '"vuoto"'
+        last_name = '"vuoto"'
         id += 1
         #tolto: last_login,date_joined
-        query = 'INSERT INTO auth_user(id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active)'+' VALUES ('+str(id)+','+password+','+is_superuser+','+username+','+first_name+','+last_name+','+email+','+is_staff+','+is_active+');'
+        query = 'INSERT INTO auth_user(id, password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)'+' VALUES ('+str(id)+','+password+','+is_superuser+','+username+','+first_name+','+last_name+','+email+','+is_staff+','+is_active+','+date_joined+');'
         out_file.write(query)
         query = '\n'
         out_file.write(query)
         #tolto: activationCode, timeCode
-        query = 'INSERT INTO createsurvey_account(user_ptr_id, type, azienda_id, survey_id) VALUES ('+str(id)+','+type+','+azienda_id+','+survey_id+');'
+        query = 'INSERT INTO createSurvey_account(user_ptr_id, type, azienda_id, survey_id,activationCode,timeCode) VALUES ('+str(id)+','+type+','+azienda_id+','+survey_id+','+activationCode+','+timeCode+');'
         out_file.write(query)
         query = '\n\n'
         out_file.write(query)
